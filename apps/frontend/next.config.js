2db46ee0bcba82929179a8ede67856b20ea5d5b2
Logans-Samantha-OS <logan.toups.11@gmail.com>
2025-11-23 11:46:26 -0600

    Simplify CSS - remove Tailwind PostCSS processing

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
   transpilePackages: [],
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  experimental: {
+    serverActions: true,
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
